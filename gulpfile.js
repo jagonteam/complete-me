36d2ffbf5d155fa3a8c65a9ef579559db5ad4104
Antoine Moser <moser.antoine@gmail.com>
2015-04-27 22:21:21 +0200

    chore: add minify-css task on gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,9 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     babel = require('gulp-babel'),
     mocha = require('gulp-mocha');
-    concat      = require('gulp-concat'),
-    uglify      = require('gulp-uglify');
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    minifyCss = require('gulp-minify-css');
 
 var paths = {
     scripts: [
@@ -16,19 +17,31 @@ var paths = {
         'server/**/*.js',
         'test/**/*.js',
         'gulpfile.js'
-  ],
-  minify: [
-    'assets/js/*.js',
-    'server.js',
-    'utils/*.js',
-    'build/*.js'
+    ],
+    minify: [
+        'assets/js/*.js',
+        'server.js',
+        'utils/*.js',
+        'build/*.js'
+    ],
+    style: [
+        'css/*.css'
+    ],
+    test: 'test/**/*.js'
+};
 
 gulp.task('minify-js', function() {
     gulp.src(paths.minify)
-    .pipe(concat('app.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist'))
+        .pipe(concat('app.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'))
 });
+
+gulp.task('minify-css', function() {
+    gulp.src(paths.style)
+        .pipe(minifyCss())
+        .pipe(gulp.dest('dist/style.css'));
+})
 
 gulp.task('verify-js', function() {
     gulp.src(['client/js/*.js'])
@@ -76,12 +89,12 @@ gulp.task('transpilation', function() {
 
 gulp.task('run-test', function() {
     gulp.src(paths.test, {
-            read: false
-        })
-        .pipe(mocha({
-            reporter: 'spec'
-        }))
-        .on('error', gutil.log);
+      read: false
+    })
+    .pipe(mocha({
+      reporter: 'spec'
+    }))
+    .on('error', gutil.log);
 });
 
 gulp.task('test', function() {
